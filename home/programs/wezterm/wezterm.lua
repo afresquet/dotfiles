f55cfffbe37e1685f6d2285db96c8fae0d26f3e4
afresquet <29437693+afresquet@users.noreply.github.com>
2024-01-28 18:39:46 +0100

    removed wezterm padding

--- home/programs/wezterm/wezterm.lua
+++ home/programs/wezterm/wezterm.lua
@@ -3,16 +3,16 @@ local wezterm = require 'wezterm'
 local xcursor_size = nil
 local xcursor_theme = nil
 
-local success, stdout, stderr = wezterm.run_child_process({ "gsettings", "get", "org.gnome.desktop.interface",
+local cursor_theme_success, cursor_theme_stdout, _ = wezterm.run_child_process({ "gsettings", "get", "org.gnome.desktop.interface",
   "cursor-theme" })
-if success then
-  xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+if cursor_theme_success then
+  xcursor_theme = cursor_theme_stdout:gsub("'(.+)'\n", "%1")
 end
 
-local success, stdout, stderr = wezterm.run_child_process({ "gsettings", "get", "org.gnome.desktop.interface",
+local cursor_size_success, cursor_size_stdout, _ = wezterm.run_child_process({ "gsettings", "get", "org.gnome.desktop.interface",
   "cursor-size" })
-if success then
-  xcursor_size = tonumber(stdout)
+if cursor_size_success then
+  xcursor_size = tonumber(cursor_size_stdout)
 end
 
 return {
@@ -20,12 +20,12 @@ return {
   window_background_opacity = 0.95,
   hide_tab_bar_if_only_one_tab = true,
   window_close_confirmation = "NeverPrompt",
-  -- window_padding = {
-  --   left = 0,
-  --   right = 0,
-  --   top = 0,
-  --   bottom = 0,
-  -- },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 
   xcursor_theme = xcursor_theme,
   xcursor_size = xcursor_size,
