09c2a79893796eb8a68d1fd545f5939f339686e0
afresquet <29437693+afresquet@users.noreply.github.com>
2024-01-31 02:57:51 +0100

    wpaperd

---
+++ home/programs/wpaperd.nix
@@ -0,0 +1,17 @@
+{ ... }:
+let
+  path = ./assets/wallpaper.png;
+in {
+  programs.wpaperd = {
+    enable = true;
+    # https://github.com/danyspin97/wpaperd#wallpaper-configuration
+    settings = {
+      DP-1 = {
+        inherit path;
+      };
+      HDMI-A-1 = {
+        inherit path;
+      };
+    };
+  };
+}
