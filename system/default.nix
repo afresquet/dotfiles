6ad7c60a95ee6ec1c8020b8bc61063ed042b251b
afresquet <29437693+afresquet@users.noreply.github.com>
2024-02-18 15:56:40 +0100

    didn't need plasma

--- system/default.nix
+++ system/default.nix
@@ -54,13 +54,6 @@
   services.xserver.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    plasma-browser-integration
-    konsole
-    oxygen
-  ];
 
   # Configure keymap in X11
   services.xserver = {
