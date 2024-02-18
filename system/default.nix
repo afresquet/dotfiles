b39c867791f37e12378393d24c47bb6bbc00da02
afresquet <29437693+afresquet@users.noreply.github.com>
2024-02-18 15:43:17 +0100

    add plasma5 for x11 environment

--- system/default.nix
+++ system/default.nix
@@ -54,7 +54,13 @@
   services.xserver.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
 
   # Configure keymap in X11
   services.xserver = {
