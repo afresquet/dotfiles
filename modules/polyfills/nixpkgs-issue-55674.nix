9debb44b32c05c52763331d1740facbaa83c9924
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-21 20:27:42 +0200

    reformat allowedUnfree polyfill

--- modules/polyfills/nixpkgs-issue-55674.nix
+++ modules/polyfills/nixpkgs-issue-55674.nix
@@ -1,12 +1,14 @@
-{ config, lib, ... }:
+{ config, lib, ... }: {
+  # https://github.com/NixOS/nixpkgs/issues/55674
 
-let
-  inherit (builtins) elem;
-  inherit (lib) getName mkOption;
-  inherit (lib.types) listOf str;
-in
-{
-  # Pending https://github.com/NixOS/nixpkgs/issues/55674
-  options.allowedUnfree = mkOption { type = listOf str; default = [ ]; };
-  config.nixpkgs.config.allowUnfreePredicate = p: elem (getName p) config.allowedUnfree;
+  options = {
+    allowedUnfree = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+  };
+
+  config = {
+    nixpkgs.config.allowUnfreePredicate = p: builtins.elem (lib.getName p) config.allowedUnfree;
+  };
 }
