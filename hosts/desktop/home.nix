6079117e8ec43cb1d8d07613be4d1206d45ee39d
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-22 20:21:53 +0200

    hyprland monitor option and touchbar config

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -8,6 +8,27 @@
   direnv.enable = true;
   git.enable = true;
   helix.enable = true;
+  hyprland.monitors = [
+    {
+      name = "DP-1";
+      width = 1920;
+      height = 1080;
+      refreshRate = 59.96;
+      x = 0;
+      y = 0;
+      enable = true;
+    }
+
+    {
+      name = "HDMI-1";
+      width = 1920;
+      height = 1080;
+      refreshRate = 59.96;
+      x = 1920;
+      y = 0;
+      enable = true;
+    }
+  ];
   mako.enable = true;
   waybar.enable = true;
   wlogout.enable = true;
--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -8,10 +8,25 @@
   direnv.enable = true;
   git.enable = true;
   helix.enable = true;
+  hyprland.monitors = [
+    {
+      name = "eDP-1";
+      width = 2256;
+      height = 1504;
+      refreshRate = 59.93;
+      x = 0;
+      y = 0;
+      scale = 2;
+      enable = true;
+    }
+  ];
   mako.enable = true;
   waybar.enable = true;
   wlogout.enable = true;
   wpaperd.enable = true;
+
+  # Services
+  touchpad.enable = true;
 
   # Shells
   carapace.enable = true;
