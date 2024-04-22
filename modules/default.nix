32390a10e298a6e4ecdaa300f5be2b9a1dd16bb1
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-22 19:16:13 +0200

    update kernel to stable

--- modules/default.nix
+++ modules/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 {
   imports = [
@@ -26,6 +26,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nix = {
     settings = {
