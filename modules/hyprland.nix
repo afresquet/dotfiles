477e39ee1a1b622f7c4c7d84855b80456090237c
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-24 23:23:46 +0200

    remove unnecessary portal

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -21,11 +21,6 @@
       xwayland.enable = true;
     };
 
-    xdg.portal = {
-      enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    };
-
-    environment.systemPackages = [ pkgs.xdg-desktop-portal-gtk ];
+    xdg.portal.enable = true;
   };
 }
