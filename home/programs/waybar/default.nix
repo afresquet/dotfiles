6bbc862f8ca1a39729a44d8931db651390190c10
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-26 13:58:29 +0200

    clock and locale

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -61,8 +61,26 @@
             };
           };
           clock = {
+            format = "{:%H:%M}";
+            format-alt = "{:%A, %B %d, %Y %R}";
             tooltip-format = "<tt><small>{calendar}</small></tt>";
-            format-alt = "{=%d-%m-%Y}";
+            calendar = {
+              mode-mon-col = 3;
+              weeks-pos = "left";
+              on-scroll = 1;
+              format = {
+                months = "<span color='#ffead3'><b>{}</b></span>";
+                days = "<span color='#ecc6d9'><b>{}</b></span>";
+                weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+                weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+                today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+              };
+            };
+            actions = {
+              "on-click-right" = "mode";
+              "on-scroll-up" = "shift_up";
+              "on-scroll-down" = "shift_down";
+            };
           };
           cpu = {
             format = "{usage}% ";
