67d9fd050c8639069ef3e2d4c656cdf4d7c10af3
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-29 11:39:49 +0200

    remove no longer necessary overlays

--- home/programs/thefuck.nix
+++ home/programs/thefuck.nix
@@ -1,25 +1,9 @@
 { lib, config, ... }: {
   options = {
     thefuck.enable = lib.mkEnableOption "thefuck";
-
-    # https://github.com/nix-community/home-manager/pull/5343
-    programs.thefuck.enableNushellIntegration = lib.mkOption {
-      default = true;
-      type = lib.types.bool;
-      description = ''
-        Whether to enable Nushell integration.
-      '';
-    };
   };
 
   config = {
     programs.thefuck.enable = config.thefuck.enable;
-
-    # https://github.com/nix-community/home-manager/pull/5343
-    programs.nushell = lib.mkIf config.programs.thefuck.enableNushellIntegration {
-      extraConfig = ''
-        alias fuck = ${config.programs.thefuck.package}/bin/thefuck $"(history | last 1 | get command | get 0)"
-      '';
-    };
   };
 }
--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -2,8 +2,6 @@
   nixpkgs.overlays = [
     (import ./brave-wayland.nix)
     (import ./discord-wayland.nix)
-    (import ./font-awesome-bump.nix)
     (import ./thefuck-nushell.nix)
-    (import ./waybar-bump.nix)
   ];
 }
