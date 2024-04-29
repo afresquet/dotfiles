dbcee414c65a3b97c19ba006595a5581682d189b
afresquet <29437693+afresquet@users.noreply.github.com>
2024-04-29 12:21:18 +0200

    add substituters and remove automatic nix garbage collection

--- modules/default.nix
+++ modules/default.nix
@@ -38,17 +38,14 @@
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
       trusted-users = [ "root" ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
     };
   };
 }
