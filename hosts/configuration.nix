c502d5a7c6135ff4ae7fead8f54b3982eb3e4e7d
afresquet <29437693+afresquet@users.noreply.github.com>
2024-10-14 10:15:56 -0300

    devenv substituter

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -62,8 +62,12 @@
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
       ];
+      trusted-substituters = [
+        "https://devenv.cachix.org"
+      ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
