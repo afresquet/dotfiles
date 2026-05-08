7bf601cd2dff8d863bcdef75ef24a4bbbe2b7318
afresquet <29437693+afresquet@users.noreply.github.com>
2026-05-08 16:58:07 +0200

    flake update

--- modules/nixos/programs/bottles.nix
+++ modules/nixos/programs/bottles.nix
@@ -14,5 +14,16 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable { environment.systemPackages = [ pkgs.bottles ]; };
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.bottles ];
+
+    # https://github.com/NixOS/nixpkgs/issues/513245#issuecomment-4320293674
+    nixpkgs.overlays = [
+      (_: prev: {
+        openldap = prev.openldap.overrideAttrs {
+          doCheck = !prev.stdenv.hostPlatform.isi686;
+        };
+      })
+    ];
+  };
 }
